dc084fec55154841806379560d38dcd02043b665
Dave Pagurek <dave@davepagurek.com>
2015-09-21 21:57:35 -0400

    Website fixes

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   title
     .on("mouseover", function(event) {
         title
-          .css("top", Math.random()*$(document).height()*1-$(document).height()*0.5)
-          .css("left", Math.random()*$(document).width()*1.2-$(document).width()*0.6);
+          .css("top", Math.random()*$(document).height())
+          .css("left", Math.random()*$(document).width());
     });
 });
