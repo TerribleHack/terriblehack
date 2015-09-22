4945c64a566990aed31299ac90fb71dae4368494
Dave Pagurek <dave@davepagurek.com>
2015-09-21 21:27:54 -0400

    Update with link to event

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
-  var signup = $("#signup");
-  signup
+  var title = $("h1");
+  title
     .on("mouseover", function(event) {
-    signup
-      .css("top", Math.random()*$(document).height()*1-$(document).height()*0.5)
-      .css("left", Math.random()*$(document).width()*1.2-$(document).width()*0.6);
-  })
-    .on("click", function() {
-    alert("You don't actually need to sign up, just follow our updates on Facebook for more info!");
-  });
+        title
+          .css("top", Math.random()*$(document).height()*1-$(document).height()*0.5)
+          .css("left", Math.random()*$(document).width()*1.2-$(document).width()*0.6);
+    });
 });
