f74bda819ceac53fa3c1fa20ad822d6cf444fb08
utk9 <utkarshlamba.2@gmail.com>
2016-02-28 23:48:24 -0500

    added floating troll face

--- js/index.js
+++ js/index.js
@@ -6,4 +6,22 @@ $(document).ready(function() {
           .css("top", Math.random()*$(document).height())
           .css("left", Math.random()*$(document).width());
     });
+
+    float();
+
 });
+
+function newPosAndSize(){
+	var left = Math.floor($(document).width()*Math.random());
+	var top = Math.floor($(document).height()*Math.random());
+	//var size = Math.floor((Math.random()*10) + 10);
+	return [left, top];
+}
+
+function float(){
+	var pos = newPosAndSize();
+	$(".floater").animate({left: pos[0], top:pos[1]}, "slow", function(){
+		float();
+	});
+
+}
