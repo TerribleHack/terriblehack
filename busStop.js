cad01027402ccb19d0fafa550edeee3ea43f4210
Dave Pagurek <davepagurek@gmail.com>
2020-04-16 20:49:00 -0700

    Initial terriblehack site

---
+++ busStop.js
@@ -0,0 +1,48 @@
+function busStop() {
+  noStroke();
+  
+  // Sign pole
+  push();
+  translate(500, -250, 50);
+  box(10, 500, 10);
+  pop();
+  
+  // Sign
+  push();
+  translate(500, -450, 75);
+  box(5, 50, 50);
+  pop();
+  
+  // Walls
+  push();
+  translate(0, -200, 0);
+  box(20, 400, 200);
+  pop();
+  
+  push();
+  translate(400, -200, 0);
+  box(20, 400, 200);
+  pop();
+  
+  // Top
+  fill(255);
+  textSize(30);
+  textAlign(CENTER);
+  text("Wait for bus", 200, -450);
+  push();
+  translate(200, -400, 0);
+  box(500, 20, 200);
+  pop();
+  
+  // Bench
+  push();
+  translate(250, -50, 0);
+  box(300, 20, 100);
+  pop();
+  
+  // Other person
+  push();
+  translate(280, 0, 0);
+  person();
+  pop();
+}
---
+++ index.js
@@ -0,0 +1,86 @@
+let position, velocity, camera;
+let lastHeading = 0;
+let font;
+
+function preload() {
+  font = loadFont('https://cdn.jsdelivr.net/npm/@typopro/dtp-e-b-garamond@3.7.5/TypoPRO-EBGaramond-Regular.ttf');
+}
+
+function setup() {
+  const canvas = createCanvas(700, 500, WEBGL);
+  canvas.parent('canvasContainer');
+
+  position = createVector(0, 0, 0);
+  velocity = createVector(0, 0, 0);
+  camera = createVector(0, 0, 0);
+  textFont(font);
+}
+
+function update() {
+  velocity.x = 0;
+  velocity.y = 0;
+  velocity.z = 0;
+  
+  if (keyIsDown(LEFT_ARROW)) { 
+    velocity.x -= 5; 
+  }
+  if (keyIsDown(RIGHT_ARROW)) { 
+    velocity.x += 5; 
+  }
+  if (keyIsDown(UP_ARROW)) { 
+    velocity.z -= 5; 
+  }
+  if (keyIsDown(DOWN_ARROW)) { 
+    velocity.z += 5; 
+  }
+  
+  position.add(velocity);
+  
+  camera.add(position.copy().sub(camera).mult(0.2));
+}
+
+function draw() {
+  update();
+  
+  background(0);
+  ambientLight(50);
+  directionalLight(255, 0, 0, 0.25, 0.25, 0);
+  pointLight(0, 0, 255, width/2, height/2, 250);
+  
+  scale(0.4);
+  rotateX(-PI/8);
+  translate(-camera.x, 100, -camera.z + 200);
+  
+  push();
+  fill(255);
+  textSize(50);
+  textAlign(CENTER);
+  text("Welcome to TerribleHack:\nReal Life", -200, -300);
+  textSize(30);
+  text("Explore with arrow keys", -200, -150);
+  pop();
+  
+  push();
+  const walking = velocity.magSq() > 0;
+  translate(position.x, position.y, position.z);
+  
+  push();
+  if (walking) {
+    lastHeading = PI/2 + atan2(-velocity.z, velocity.x);
+  }
+  rotateY(lastHeading);
+  person(walking);
+  pop();
+  
+  translate(0, -250, 0);
+  textSize(24);
+  fill(255);
+  textAlign(CENTER);
+  text("YOU", 0, 0, 0);
+  pop();
+  
+  push();
+  translate(300, 0, 300);
+  busStop();
+  pop();
+}
