69b79d23175e84de2c50493284c5f4c6c56a7dae
Dave Pagurek <davepagurek@gmail.com>
2026-03-31 19:10:29 -0400

    Update site, new google form

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const getLink = () => {
   const name = encodeURIComponent(nameInput.value);
   const email = encodeURIComponent(emailInput.value);
 
-  return `https://docs.google.com/forms/d/e/1FAIpQLScMyg56oS2CEknFhEurWTl8OSXcEQW-7NrLT5rU9JNzYWokZw/viewform?usp=pp_url&entry.498838656=${name}&entry.932453923=${email}`;
+  return `https://docs.google.com/forms/d/e/1FAIpQLSdzheaLsrUu-LhgnUhfoD6XvucNE_XIqUm8qyu767DgN3EzHw/viewform?usp=pp_url&entry.498838656=${name}&entry.932453923=${email}`
 };
 
 [nameInput, emailInput].forEach(input => {
